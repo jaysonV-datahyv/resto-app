4dedd2cbc7f8fd2de0676e50b4169cdb1fbc3510
jaysonV-datahyv <jayson@datahyv.com>
2024-07-04 04:37:25 +0800

    LogIn Field Margin Adjustment

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -5,7 +5,7 @@
                 <p class="mt-5 text-2xl font-bold drop-shadow-2xl border-black">Welcome to</p>
                 <h1 class="xl:text-8xl text-6xl font-bold mb-12 drop-shadow-2xl">EZ Eats</h1>
             </div>
-            <div class="mt-12">
+            <div class="mt-7">
                 <Panel class="shadow-2xl xl:p-4 sm:p-2 xl:w-1/4 md-1/2 sm:w-full xl:m-auto sm:m-0">
                     <div class="rounded-full bg-white -mt-32 inline-flex shadow-lg p-4 border border-slate-300">
                         <Image src="/images/ez_eats_logo.png" alt="EZ Eats Logo" class="xl:w-48 lg:w-44 md:w-40 w-32" />
