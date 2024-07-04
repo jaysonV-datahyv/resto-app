e24d32aa1a432c8d60e4fd047a4a59fe551fc0d1
jaysonV-datahyv <jayson@datahyv.com>
2024-07-05 04:49:55 +0800

    update constances

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,6 @@
 export const API_BASE_URL = "https://admin.ezeats-resto.com";
 export const API_BASE_IMAGE_URL = `${API_BASE_URL}/public/images`;
+import { ref } from 'vue';
 // export const API_BASE_URL = "http://127.0.0.1:8000";
 // export const API_BASE_IMAGE_URL = `${API_BASE_URL}/images`;
+export const USER_SESSION = ref(null);
